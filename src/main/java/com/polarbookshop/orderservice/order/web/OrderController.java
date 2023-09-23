968189ee949fdc8ee3e2f36041f4a67f2b0c9f90
Chien Nguyen <36409877+fankao@users.noreply.github.com>
2023-09-23 10:16:02 +0700

    Observability and monitoring

--- src/main/java/com/polarbookshop/orderservice/order/web/OrderController.java
+++ src/main/java/com/polarbookshop/orderservice/order/web/OrderController.java
@@ -2,6 +2,8 @@ package com.polarbookshop.orderservice.order.web;
 
 import com.polarbookshop.orderservice.order.domain.Order;
 import com.polarbookshop.orderservice.order.domain.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("orders")
 public class OrderController {
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
     private final OrderService orderService;
 
     public OrderController(OrderService orderService) {
@@ -18,10 +21,12 @@ public class OrderController {
     }
     @GetMapping
     public Flux<Order> getAllOrders(@AuthenticationPrincipal Jwt jwt){
+        log.info("Fetching all orders");
         return orderService.getAllOrders(jwt.getSubject());
     }
     @PostMapping
     public Mono<Order> submitOrder(@RequestBody OrderRequest orderRequest){
+        log.info("Order for {} copies of the book with ISBN {}", orderRequest.quantity(), orderRequest.isbn());
         return orderService.submitOrder(orderRequest.isbn(), orderRequest.quantity());
     }
 }
